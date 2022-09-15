72eaac8180ab6df9ef3a919009a970695c761e55
toddlewis-studio <todd.lewis.art@gmail.com>
2022-09-14 23:30:43 -0600

    push file called

--- www/script/app.js
+++ www/script/app.js
@@ -1,26 +1,72 @@
 import i0 from './i0.js'
 
+const container = (...vo) =>
+  i0.vo`div`
+    .style`container`
+    .child ( ...vo )
+
 const card = (...vo) =>
   i0.vo`div`
     .style`card`
     .child
-    ( i0.vo`div`
-	.style`card-body`
-	.child (...vo)
-    )
+       ( i0.vo`div`
+	         .style`card-body`
+	         .child (...vo)
+       )
 
 const counter = () =>
   i0.vo`button`
+    .style`btn btn-primary`
     .on`click::increment`
     .data('Counter: {0}', 'counter')
 
 let view = i0.view (
-  { counter: 0 },
-  [ card
-      ( counter ()
+  { counter: 0, title: 'Welcome to i07' },
+  [ container
+      ( card
+          ( i0.vo`h1`
+	            .data('{0}', 'title')
+              , counter ()
+          )
       )
   ],
   { increment: model => console.log( model.set`counter`(model.get`counter` + 1) )
+  }
+)
+
+view = i0.view (
+  { todos: []
+  , todoVal: ''
+  },
+  [ container
+      ( card
+          ( i0.vo`h1`.text`Todos`
+          , card
+            ( i0.vo`input`
+                .attr`placeholder=Enter your todo...`
+                .attr`title=Enter your todo...`
+                .bind`todoVal`
+            , i0.vo`button`
+                .text`Create`
+                .on`click::createTodo`
+            )
+          , card
+            ( i0.vo`div`
+                .list`todos::t`
+                .style`card`
+                .child
+                  ( i0.vo`div`
+                      .style`card-body`
+                      .data('{0}', 't')
+                  )
+            )
+          )
+      )
+  ],
+  { createTodo: m => {
+      m.todos.push({val: m.todoVal})
+      m.todoVal = ''
+    }
   }
 )
 
