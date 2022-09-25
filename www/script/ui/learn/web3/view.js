09d1f31dfe0dbd9c6c6a7805a1a3d4f2e66b67c1
Todd Lewis aka Brayan <brayanbyrdsong@icloud.com>
2022-09-25 11:43:40 -0600

    stuff

--- www/script/ui/learn/web3/view.js
+++ www/script/ui/learn/web3/view.js
@@ -5,6 +5,18 @@ import _ui from './_ui.js'
 export default () => {
   const ui = { ..._ui }
         
+  const headerWBtns = (text, ...btns) =>
+    ui.div
+      ( i0.vo`h4`
+          .text`${text}`
+          .style`m-0`
+      , ui.div
+          ( ...btns
+          )
+          .style`btn-group ms-4`
+      )
+      .style`d-flex align-items-center my-4`
+
   const view = i0.view(
     { cryptoName: ''
     , tkview: 'wallet'
@@ -14,45 +26,47 @@ export default () => {
             .style`text-light text-center`
             .text`Web 3 - Solana`
         , ui.card
-            ( i0.vo`h4`
-                .text`Phantom Wallet`
-            , ui.btn`connectWallet::primary m-1::Connect Wallet`
-            , ui.btn`messageWallet::primary m-1::Message Wallet`
+            ( headerWBtns( 'Phantom Wallet'
+                , ui.btn`connectWallet::primary::Connect Wallet`
+                , ui.btn`messageWallet::primary::Message Wallet`
+              )
             )
         , i0.vo`br`
         , ui.card
-            ( i0.vo`h4`.text`Wallet`
-            , ui.btn`newWallet::success m-1::Create Wallet`
-            , ui.btn`loadWallet::primary m-1::Load Wallet`
-            , ui.div
-                ( i0.vo`h4`
-                    .text`Crypto`
-                , ui.div
-                    ( ui.div
-                      ( ui.btn`tkviewWallet::outline-primary::Wallet`
-                          .data`tkview`((vo, tkview) => {
-                            vo.el.classList[tkview === 'wallet' ? 'remove' : 'add']('btn-outline-primary')
-                            vo.el.classList[tkview === 'wallet' ? 'add' : 'remove']('btn-primary')
-                          })
-                      , ui.btn`tkviewCreate::outline-success::Create`
-                          .data`tkview`((vo, tkview) => {
-                            vo.el.classList[tkview === 'create' ? 'remove' : 'add']('btn-outline-success')
-                            vo.el.classList[tkview === 'create' ? 'add' : 'remove']('btn-success')
-                          })
-                      )
-                      .style`btn-group`
-                    )
-                    .style`ms-4`
-                )
-                .style`d-flex align-items-center`
-            , i0.vo`form`
-                .style`input-group`
+            ( headerWBtns( 'Solana Wallet'
+                , ui.btn`newWallet::success::Create Wallet`
+                , ui.btn`loadWallet::primary::Load Wallet`
+              )
+            , headerWBtns( 'Crypto Tokens'
+                , ui.btn`tkviewWallet::outline-primary::Wallet`
+                    .data`tkview`((vo, tkview) => {
+                      vo.el.classList[tkview === 'wallet' ? 'remove' : 'add']('btn-outline-primary')
+                      vo.el.classList[tkview === 'wallet' ? 'add' : 'remove']('btn-primary')
+                    })
+                , ui.btn`tkviewCreate::outline-success::Create`
+                    .data`tkview`((vo, tkview) => {
+                      vo.el.classList[tkview === 'create' ? 'remove' : 'add']('btn-outline-success')
+                      vo.el.classList[tkview === 'create' ? 'add' : 'remove']('btn-success')
+                    })
+              )
+            , i0.vo`div`
+                .data`tkview`((vo, tkview) => {
+                  vo.el.classList[tkview === 'wallet' ? 'remove' : 'add']('d-none')
+                })
+                .child
+                  ( i0.vo`p`.text`You have no wallet`
+                  )
+            , i0.vo`div`
                 .data`tkview`((vo, tkview) => {
                   vo.el.classList[tkview === 'create' ? 'remove' : 'add']('d-none')
                 })
                 .child
-                  ( ui.input`cryptoName::Token Name`
-                  , ui.btn`createToken::success::Create Token`
+                  ( i0.vo`form`
+                      .style`input-group`
+                      .child
+                        ( ui.input`cryptoName::Token Name`
+                        , ui.btn`createToken::success::Create Token`
+                        )
                   )
             )
         )
